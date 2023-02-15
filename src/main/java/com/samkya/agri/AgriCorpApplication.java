9936cfb1670bd03381768ba5be0d37afb150e262
nkamesh <n.kamesh@yahoo.com>
2023-02-15 23:52:05 +0530

    Initial commit

---
+++ src/main/java/com/samkya/agri/AgriCorpApplication.java
@@ -0,0 +1,13 @@
+package com.samkya.agri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriCorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriCorpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samkya/agri/AgriCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samkya.agri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
